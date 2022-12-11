99ec9ad83dea98a06cf5d8fd7043cc07ecb81acc
acan411 <opsitesky@163.com>
2022-12-12 01:09:17 +0800

    update

--- TestProject/TestProject/src/main/java/priv/acan/Huawei.java
+++ TestProject/TestProject/src/main/java/priv/acan/Huawei.java
@@ -1,12 +1,8 @@
 package priv.acan;
 
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
-
 public class Huawei {
 
     public static void main(String[] args) {
-
+        System.out.println("github666");
     }
 }
