91146c351869448cfc73fb112a1bc14a4bb7eedd
acan411 <opsitesky@163.com>
2022-12-12 00:59:07 +0800

    update

---
+++ test/test/src/main/java/priv/test/MyTest.java
@@ -0,0 +1,14 @@
+package priv.test;
+
+import org.junit.Test;
+
+public class MyTest {
+
+    @Test
+    public void asdf(){
+        System.out.println(9<<2);
+        System.out.println(9>>2);
+        System.out.println(-9>>>2);
+        System.out.println(-9>>2);
+    }
+}
