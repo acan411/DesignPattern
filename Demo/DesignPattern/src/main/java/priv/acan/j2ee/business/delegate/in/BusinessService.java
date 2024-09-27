5064d505798ce0d0b9769cf5bd7617b0a05b7eb8
acan <opsitesky@163.com>
2024-09-27 09:09:13 +0800

    change dir

--- Demo/DesignPattern/src/main/java/priv/acan/j2ee/business/delegate/in/BusinessService.java
+++
@@ -1,9 +0,0 @@
-package priv.acan.j2ee.business.delegate.in;
-
-/**
- * @author acan
- * @version 2023/03/04 14:40
- */
-public interface BusinessService {
-    void doProcessing();
-}
--- Demo/DesignPattern/src/main/java/priv/acan/structural/adapter/in/advance/AdvancedMediaPlayer.java
+++
@@ -1,8 +0,0 @@
-package priv.acan.structural.adapter.in.advance;
-
-public interface AdvancedMediaPlayer {
-
-    void playVlc(String fileName);
-
-    void playMp4(String fileName);
-}
